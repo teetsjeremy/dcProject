115477841dcd87709b480cebb4e1b3b9d28098aa
Ubuntu <ubuntu@ip-172-31-20-176.us-west-2.compute.internal>
2019-11-28 04:30:34 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'dcProject'
+app.config["MONGO_URI"] = 'mongodb+srv://Teets:Lexi1013@myfirstcluster-oq2u3.mongodb.net/dcProject?retryWrites=true&w=majority'
+
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("main.html")
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
