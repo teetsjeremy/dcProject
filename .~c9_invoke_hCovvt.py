683f838dd5cef7f1e4f54d81faa4b3840c524b69
Ubuntu <ubuntu@ip-172-31-20-176.us-west-2.compute.internal>
2019-11-28 07:09:41 +0000

    created main.html and connected DB

--- .~c9_invoke_hCovvt.py
+++ .~c9_invoke_hCovvt.py
@@ -1,20 +1,18 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+
+
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'dcProject'
 app.config["MONGO_URI"] = 'mongodb+srv://Teets:Lexi1013@myfirstcluster-oq2u3.mongodb.net/dcProject?retryWrites=true&w=majority'
 
-mongo = PyMongo(app)
-
 
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("main.html", tasks=mongo.db.E9Xissues.find())
+    return render_template("main.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
